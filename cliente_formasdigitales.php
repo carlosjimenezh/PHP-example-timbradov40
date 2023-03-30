8573df2f268ff8f04749a0987aa728b05de6ac8d
Formas Digitales <43219929+formasdigitales@users.noreply.github.com>
2023-03-30 09:42:39 -0600

    Add files via upload

---
+++ cliente_formasdigitales.php
@@ -0,0 +1,69 @@
+<?php
+    class ClienteFormasDigitales{
+        
+        public $xml;
+        public $cadenaoriginalxslt;
+
+        public function __construct($xmlpath){
+            //Constructor de la clase
+            $this->xml = new DOMDocument();
+            $this->xml = simplexml_load_file($xmlpath) or die("XML invalido");
+            $this->cadenaoriginalxslt = dirname(__FILE__)."/resources/cadenaoriginal_4_0.xslt";
+        }
+
+        public function loadCertKey($filecer,$fileKey){        
+            //carga certificado
+            $data_cert = openssl_x509_parse(file_get_contents($filecer));
+            $serial = $this->getNoCertificado($data_cert['serialNumberHex']); //Obtiene numero certificado y lo carga al xml  
+            $this->xml['NoCertificado'] = $serial;
+            
+            $cert = file_get_contents(dirname(__FILE__) . "/resources/ESCUELA_KEMPER_URGATE_EKU9003173C9.cer");
+            $certificado = str_replace(array('\n', '\r'), '', base64_encode($cert));
+            $this->xml['Certificado'] = $certificado;//Carga el certificado en base64
+            $this->xml->saveXML();
+            $cadenaorignal =$this->generaCadenaOriginal();//Obtiene la cadena original
+            $private = openssl_pkey_get_private(file_get_contents($fileKey));
+            openssl_sign($cadenaorignal,$signature,$private,"sha256WithRSAEncryption");
+            $sello = base64_encode($signature);
+            $this->xml['Sello'] = $sello;//Carga el sello en el xml 
+
+            return $this->xml->saveXML();//Regresa el xml con los datos actualizados
+
+        }
+
+        public function timbrarXML($parametros){
+            $client = new SoapClient('http://dev33.facturacfdi.mx/WSTimbradoCFDIService?wsdl');//Crea cliente soap del webservice de formas digitales
+		    return $client->TimbrarCFDI($parametros);//Realiza el llamado al método de timbrado.
+        }
+
+        public function generaCadenaOriginal(){
+            $XSL = new DOMDocument();
+            $XSL->load($this->cadenaoriginalxslt);
+            $proc = new XSLTProcessor();
+            @$proc->importStylesheet($XSL);
+            $cadena = $proc->transformToXml($this->xml);
+            return $cadena;
+        }
+
+        public function getDate(){
+            $date = date('Y-m-d H:i:s');
+            $this->xml['Fecha']=str_replace(' ','T',$date);
+        }
+        
+        function getNoCertificado($serial){
+			$noCertificado = "";
+			
+			if((strlen($serial) % 2) == 1){
+				$serial = " " . $serial;
+			}
+
+			for($i=0; $i < strlen($serial)/2; $i++){
+				$aux = substr($serial, $i*2, ($i * 2) + 2);
+				$noCertificado .=  substr($aux,1,1);
+			}
+
+			return $noCertificado;
+		}
+
+    }
+?>
---
+++ index.php
@@ -0,0 +1,49 @@
+<?php 
+    require_once "cliente_formasdigitales.php";
+    header('Content-type: text/html;charset=utf-8');
+    try{
+      set_time_limit(0);
+      date_default_timezone_set("America/Mexico_City");
+
+      $filexml = dirname(__FILE__) . "/resources/cfdi_v40_generico.xml";
+      $filecer = dirname(__FILE__) . "/resources/CSD_Escuela_Kemper_Urgate_EKU9003173C9_20190617_131753s_certificado.pem";
+      $filekey = dirname(__FILE__) . "/resources/CSD_Escuela_Kemper_Urgate_EKU9003173C9_20190617_131753_llavePrivada.pem";
+
+      $clienteFD = new ClienteFormasDigitales($filexml);
+      $clienteFD->getDate();
+      
+
+      $autentica = new Autenticar();
+		  $autentica->usuario = "pruebasWS";
+		  $autentica->password = "pruebasWS"; //se crea el objeto autenticar con el usuario y contraseña del servicio de formas digitales 
+      
+      $parametros = new Parametros();
+      $parametros->accesos = $autentica;
+      $parametros->comprobante=$clienteFD->loadCertKey($filecer,$filekey);
+
+      $responseTimbre = $clienteFD->timbrarXML($parametros);//se llama al método timbrarXML y se mandan los parametros (acessos y comprobante)
+
+      if(isset($responseTimbre->acuseCFDI->error)){ //Si ocurrio un error al momento de timbrar el xml enviado se mostrará en pantalla
+        echo "codigoErr: " . $responseTimbre->acuseCFDI->error. "<br>";
+      }
+  
+      if($responseTimbre->acuseCFDI->xmlTimbrado){//Si el xml fue timbrado con exito se mostrará en pantalla
+        echo 'XML TMIBRADO:<BR> <textarea>' . $responseTimbre->acuseCFDI->xmlTimbrado . '</textarea>';
+      }
+
+
+    }catch(Exception $ex){
+      print("Error: $ex");
+    }
+
+    class Autenticar{
+      public $usuario;
+      public $password;
+    }
+    
+    
+    class Parametros{
+      public $accesos;
+      public $comprobante;
+    }
+?>
